f10559b8e765d51e571821a3a8a7de388c93c58d
Anti Spamer <C:\Users\ruben\AppData\Roaming\The Bat!>
2021-05-18 11:59:44 +0400

    tasks done

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,13 @@ import edit from './assets/images/edit.png'
 
 const App = () => {
   const [users, setUsers] = useState(null);
+  const [modalChanges, setModalChanges] = useState(null);
   const [query, setQuery] = useState(0);
   const [show, setShow] = useState(false);
+  const [modalDataName, setModalDataName] = useState()
+  const [modalDataEmail, setModalDataEmail] = useState()
+  const [modalDataPhone, setModalDataPhone] = useState()
+  const [modalDataId, setModalDataId] = useState()
   const getData = () => {
     setUsers(null)
     fetch(`https://jsonplaceholder.typicode.com/users?_start=${query}&_limit=5`)
@@ -34,6 +39,19 @@ const App = () => {
         setUsers(JSON.parse(localStorage.getItem(`users${query}`)))
       }, 700))
   }
+  const onClose = (e) =>{
+    setShow(false)
+    setUsers(JSON.parse(localStorage.getItem(`users${query}`)))
+    setModalChanges(JSON.parse(localStorage.getItem(`users${query}`)))
+  }
+  const openModal = (e) => {
+    setShow(true)
+    setModalChanges(users)
+    setModalDataId(JSON.parse(e.target.value).id)
+    setModalDataName(JSON.parse(e.target.value).name)
+    setModalDataEmail(JSON.parse(e.target.value).email)
+    setModalDataPhone(JSON.parse(e.target.value).phone)
+  }
   const handleLikes = (e) => {
     setUsers(users.filter((user, i, arr) => {
       if (+user.id === +JSON.parse(e.target.value).id) {
@@ -45,13 +63,33 @@ const App = () => {
   }
 
   const handleName = (e) => {
-    setUsers(users.filter((user, i, arr) => {
-      if (user.id === i) {
-        user.name = e.target.value;
+    setModalChanges(users.filter((user, i, arr) => {
+      if (+user.id === +e.target.id) {
+        setModalDataName(e.target.value)
+        arr[i].name = e.target.value;
       }
       return (arr)
     }))
-    localStorage.setItem(`users${query}`, JSON.stringify(users))
+  }
+  const handleEmail = (e) => {
+    setModalChanges(users.filter((user, i, arr) => {
+      if (+user.id === +e.target.id) {
+        setModalDataEmail(e.target.value)
+        arr[i].email = e.target.value;
+      }
+      return (arr)
+    }))
+
+  }
+
+  const handlePhone = (e) => {
+    setModalChanges(users.filter((user, i, arr) => {
+      if (+user.id === +e.target.id) {
+        setModalDataPhone(e.target.value)
+        arr[i].phone = e.target.value;
+      }
+      return (arr)
+    }))
   }
   const handleDislike = (e) => {
     setUsers(users.filter((user, i, arr) => {
@@ -62,6 +100,12 @@ const App = () => {
     }))
     localStorage.setItem(`users${query}`, JSON.stringify(users))
   }
+
+  const saveData = () => {
+    localStorage.setItem(`users${query}`, JSON.stringify(modalChanges))
+      setShow(false)
+  }
+  
   useEffect(() => {
     getData()
   }, [query])
@@ -79,33 +123,37 @@ const App = () => {
         <div className="title-grid">
           <div className="font-weight-bold">Image</div>
           <div className="font-weight-bold">Full Name</div>
-          <div className="font-weight-bold">Phone</div>
           <div className="font-weight-bold">Email</div>
+          <div className="font-weight-bold">Phone</div>
           <div className="font-weight-bold">Likes</div>
           <div className="font-weight-bold">Dislikes</div>
+          <div className="font-weight-bold">Edit</div>
         </div>
         {users ? users.map((user, index) =>
           <div key={user.name} className="users-grid">
             <div className="user-item user-image font-weight-normal">
-              {user.name.split(' ')[0].slice(0, 1).toUpperCase()} */}
-              {user.name.split(' ')[1].slice(0, 1).toUpperCase()}
+              {user.name.includes(' ') ? user.name.split(' ')[0].slice(0, 1).toUpperCase() : user.name.slice(0,1).toUpperCase()} 
+              {user.name.includes(' ') ? user.name.split(' ')[1].slice(0, 1).toUpperCase() : ''}
             </div>
             <div className="user-item user user-name font-weight-normal">{user.name}</div>
-            <div className="user-item font-weight-normal">{user.phone}</div>
             <div className="user-item font-weight-normal">{user.email}</div>
+            <div className="user-item font-weight-normal">{user.phone}</div>
             <Likes handleLikes={handleLikes} className="like-btn" value={JSON.stringify(user)} like={user.like} />
             <Dislikes handleDislike={handleDislike} className="like-btn" value={JSON.stringify(user)} dislike={user.dislike} />
-            <button className="edit-btn" onClick={() => { setShow(true) }} >
-              <img src={edit} alt="" />
+            <button className="edit-btn" id={user.id} value={JSON.stringify(user)} onClick={openModal} >
+              <img src={edit} alt="" style={{pointerEvents: 'none'}}/>
             </button>
             <EditModal
-              index={index}
-              userName={user.name}
-              userMail={user.email}
-              userPhone={user.phone}
-              onClose={() => { setShow(false) }}
+              userIndex = {modalDataId}
+              userName={modalDataName}
+              userEmail={modalDataEmail}
+              userPhone={modalDataPhone}
+              onClose={onClose}
               show={show}
               handleName={handleName}
+              handleEmail={handleEmail}
+              handlePhone={handlePhone}
+              saveData={saveData}
             />
           </div>
         ) : <Loader />}
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,21 +5,26 @@ const EditModal = (props) => {
 
     const onSubmit = e => {
         e.preventDefault();
-     };
-
+    };
     return (
 
         <form onSubmit={onSubmit}>
             <div className={`edit-modal-main ${props.show ? 'show' : ''}`} onClick={props.onClose}>
                 <div className="edit-modal-body" onClick={e => e.stopPropagation()}>
-                    <button className="close-btn"  onClick={props.onClose}>🗙</button>
-                    <label>Name</label>
-                    <input type="text" value={props.userName} index = {props.index} onChange={props.handleName} />
-                    <label>E-Mail</label>
-                    <input type="text" value={props.userMail} onChange={props.handleName}/>
-                    <label>Phone</label>
-                    <input type="text" value={props.userPhone} onChange={props.handleName}/>
-                    <button className = "btn btn-success" type="submit" onClick={props.onSubmit}>Save</button>
+                    <button className="close-btn" onClick={props.onClose}>🗙</button>
+                    <div className="input-blocks mt-5">
+                        <label>Name</label>
+                        <input className="input-forms" placeholder="Enter name" type="text" value={props.userName} id={props.userIndex} onChange={props.handleName} />
+                    </div>
+                    <div className="input-blocks">
+                        <label>E-Mail</label>
+                        <input className="input-forms" placeholder="Enter email" type="email" value={props.userEmail} onChange={props.handleEmail} id={props.userIndex} />
+                    </div>
+                    <div className="input-blocks">
+                        <label>Phone</label>
+                        <input className="input-forms" placeholder="Enter phone" type="tel" value={props.userPhone} id={props.userIndex} onChange={props.handlePhone} />
+                    </div>
+                    <button className="btn btn-success mt-3" type="submit" onClick={props.saveData}>Save</button>
                 </div>
             </div>
         </form>
